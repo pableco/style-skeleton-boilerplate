cb9b4eab99eccd71616ddfad5894ac4fa9eb1bb7
Pablo Grillo <pablo.grillo@roiback.com>
2018-03-09 12:10:10 +0100

    js class test case

--- example/common-hover.js
+++ example/common-hover.js
@@ -1,14 +1,17 @@
 /* eslint-disable */
+
+// this example is for js--common isuues in lists
+
 $(function() {
     $(".js--hover").hover(
         function () {
-            console.log('add class');
+            // console.log('add class');
             var classes = $(this).attr('class').split(' ');
             var bemClass = classes[0];
             $(this).addClass(bemClass + '--hover');
         },
         function () {
-            console.log('remove class');
+            // console.log('remove class');
             var classes = $(this).attr('class').split(' ');
             var bemClass = classes[0];
             $(this).removeClass(bemClass + '--hover');
